b888547a42e662fb147187bc0820ba25940a727a
Bharath kumar Sallla <bharathsalla456@gmail.com>
2026-06-25 11:40:52 +0530

    Add base path configuration to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,7 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  vite: {                          // 👈 ADD THIS
+    base: '/ultimate-bank-tool/',  // 👈 ADD THIS
+  },                               // 👈 ADD THIS
 });
